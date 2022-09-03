01e361187112aa2173ca21ffd6d4f5865a654a3c
Federico <fedevitale99@gmail.com>
2022-09-03 03:06:25 +0200

    feat: code cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 //! src/main.rs
 use hawk::log;
 use hawk::models::config::Config;
+use hawk::models::workspace::Workspace;
 use hawk::utils;
 use hawk::watchers;
 use std::fs;
@@ -8,18 +9,21 @@ use std::fs;
 use clap::Parser;
 use colored::*;
 
+#[derive(Parser, Debug)]
+struct CopyFlags {
+    #[clap(short, long, value_parser, default_value_t = false)]
+    watch: bool,
+}
+
 #[derive(clap::Subcommand, Debug)]
 enum Action {
     Clean,
-    Copy,
+    Copy(CopyFlags),
 }
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about)]
 struct Args {
-    #[clap(short, long, value_parser, default_value_t = false)]
-    watch: bool,
-
     #[clap(short, long, value_parser)]
     config: Option<String>,
 
@@ -47,13 +51,18 @@ fn main() -> notify::Result<()> {
 
     let config = Config::load(&config_file).expect("Could not read config file");
 
-    println!(
-        "{} {}",
-        "Loaded config file".dimmed(),
-        config_file.dimmed().underline()
-    );
-
     match args.action {
+        None => {
+            for mut workspace in config.workspaces {
+                let target = config.target.clone();
+
+                if let Err(e) = workspace.load_name_if_possible() {
+                    log::error("An error has occurred:", e);
+                }
+
+                copy_workspace_files(&mut workspace, &target)?;
+            }
+        }
         Some(Action::Clean) => {
             for mut workspace in config.workspaces {
                 let target = config.target.clone();
@@ -88,90 +97,90 @@ fn main() -> notify::Result<()> {
                     }
                 }
             }
-        }
-        None | Some(Action::Copy) => {
-            if !args.watch {
-                let mut skipped = 0;
-
-                for mut workspace in config.workspaces {
-                    let target = config.target.clone();
-
-                    if let Err(e) = workspace.load_name_if_possible() {
-                        log::error("An error has occurred:", e);
-                    }
-
-                    println!(
-                        "Copying [{}]({}) to {}",
-                        workspace.name.blue(),
-                        workspace.path.underline().dimmed(),
-                        target.blue()
-                    );
-                    if let Ok(content) = fs::read_dir(&workspace.path) {
-                        for f in content {
-                            match f {
-                                Ok(path) => {
-                                    if let Some(filename) =
-                                        path.path().file_name().unwrap().to_str()
-                                    {
-                                        if path.path().is_file() && is_yaml(filename) {
-                                            utils::copy_file(
-                                                &path.path(),
-                                                &target,
-                                                &workspace.name,
-                                            )?
-                                        } else {
-                                            println!(
-                                                "Skipping: {:?} {}",
-                                                path.path().display(),
-                                                path.path()
-                                                    .file_name()
-                                                    .unwrap()
-                                                    .to_str()
-                                                    .unwrap()
-                                                    .ends_with("yml")
-                                            );
-                                            skipped += 1;
-                                        }
-                                    }
-                                }
-                                Err(err) => println!("failed to copy: {}", err),
-                            }
-                        }
-                    }
+        },
+        Some(Action::Copy(f)) => {
+            let handle = std::thread::spawn(move || {
+                if !f.watch {
+                    return;
                 }
 
-                if skipped > 0 {
-                    println!("Skipped {} files.", skipped);
+                if let Err(err) = watchers::watch_config(&config_file) {
+                    log::error("An error has occurred:", err)
+                }
+            });
+
+            for mut workspace in config.workspaces {
+                let target = config.target.clone();
+
+                if let Err(e) = workspace.load_name_if_possible() {
+                    log::error("An error has occurred:", e);
                 }
 
-                return Ok(());
-            }
+                copy_workspace_files(&mut workspace, &target)?;
 
-            if args.watch {
-                let handle = std::thread::spawn(move || {
-                    if let Err(err) = watchers::watch_config(&config_file) {
-                        log::error("An error has occurred:", err)
-                    }
-                });
-
-                for mut workspace in config.workspaces {
-                    let target = config.target.clone();
-
-                    match workspace.load_name_if_possible() {
-                        Ok(_) => {}
-                        Err(e) => log::error("An error has occurred:", e),
-                    }
-
+                if f.watch {
+                    println!("\n");
                     std::thread::spawn(move || {
                         if let Err(err) = watchers::watch_sync(workspace, &target) {
                             log::error("Something went wrong:", err)
                         }
                     });
                 }
+            }
 
-                handle.join().unwrap();
+            handle.join().unwrap();
+        }
+    }
+
+    Ok(())
+}
+
+
+fn copy_workspace_files(workspace: &mut Workspace, target: &str) -> notify::Result<()> {
+    let mut skipped = 0;
+
+    println!(
+        "Copying [{}]({}) to {}",
+        workspace.name.blue(),
+        workspace.path.underline().dimmed(),
+        target.blue()
+        );
+
+    if let Ok(content) = fs::read_dir(&workspace.path) {
+        for f in content {
+            match f {
+                Ok(path) => {
+                    if let Some(filename) =
+                        path.path().file_name().unwrap().to_str()
+                        {
+                            if path.path().is_file() && is_yaml(filename) {
+                                utils::copy_file(
+                                    &path.path(),
+                                    target,
+                                    &workspace.name,
+                                    )?
+                            } else {
+                                println!(
+                                    "Skipping: {:?} {}",
+                                    path.path().display(),
+                                    path.path()
+                                    .file_name()
+                                    .unwrap()
+                                    .to_str()
+                                    .unwrap()
+                                    .ends_with("yml")
+                                    );
+                                skipped += 1;
+                            }
+                        }
+                }
+                Err(err) => println!("failed to copy: {}", err),
             }
         }
+    }
+
+    if skipped > 0 {
+        println!("Skipped {} files.", skipped);
     }
 
     Ok(())
